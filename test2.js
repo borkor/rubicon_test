b44a4ebf26bed64d3ce8f382e9a5c282af11f4b8
Borko Rastovic <borko.rastovic@gmail.com>
2014-05-26 19:33:00 +0200

    commit 2

--- test2.js
+++ test2.js
@@ -1,8 +1,8 @@
 function init() 
 { 
  // The method “getElem” can be used to get an item on the page by its id 
-// getElem('id', 'contactBlock', 0).style.display="none"; 
-// getElem('id', 'userInfoBlock', 0).style.display="none"; 
-// getElem('id', 'addressBlock', 0).style.display="none"; 
- 
+getElem('id', 'contactBlock', 0).style.display="none"; 
+getElem('id', 'userInfoBlock', 0).style.display="none"; 
+getElem('id', 'addressBlock', 0).style.display="none"; 
+
 } 
