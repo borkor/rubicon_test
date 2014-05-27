4da77cd1228f07702f4ddd101852bacfd521388d
Borko Rastovic <borko.rastovic@gmail.com>
2014-05-27 22:22:59 +0200

    commit 2

--- test2.js
+++ test2.js
@@ -1,6 +1,6 @@
 
 function init() { 
-	getElem('id', 'contactBlock', 0).style.display="none"; 
-	getElem('id', 'userInfoBlock', 0).style.display="none"; 
-	getElem('id', 'addressBlock', 0).style.display="none"; 
+//	getElem('id', 'contactBlock', 0).style.display="none"; 
+//	getElem('id', 'userInfoBlock', 0).style.display="none"; 
+//	getElem('id', 'addressBlock', 0).style.display="none"; 
 } 
